2d471a25d84d5acd97d2df09ad468c73021ff51d
Eduardo <eduardogm33@outlook>
2026-06-24 17:07:09 +0100

    style: modificado estilo del panel de administración. Las opciones de acceso a las diferentes áreas de gestión ahora son tarjetas que redirigen al usuario a las áreas correspondientes

--- resources/views/auth/dashboard.blade.php
+++ resources/views/auth/dashboard.blade.php
@@ -5,5 +5,23 @@
         Welcome to the dashboard {{ $user->name }}
     </h1>
 
-    <x-button href="{{ route('portfolios-index', $user->id) }}">Mis Portfolios</x-button>
+    <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+            <a href='{{ route('portfolios-index', $user->id) }}', class="block">
+                <div class="flex flex-inline items-center bg-gray-500 rounded-lg shadow-sm hover:shadow-md transition-shadow duration-200 p-6">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z" />
+                </svg>
+                <p class="text-white text-center text-lg mx-2">Cartera de clientes<p>
+                </div>
+            </a>
+
+            <a href='{{route('manager-editions', $user->id)}}' class="block">
+                <div class="flex flex-inline items-center bg-gray-500 rounded-lg shadow-sm hover:shadow-md transition-shadow duration-200 p-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                    </svg>
+                <p class="text-white text-center text-lg mx-2">Eventos/Ediciones</p>
+                </div>
+            </a>
+    </div>
 </x-layout>
