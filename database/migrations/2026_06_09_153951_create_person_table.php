e4a7839187187bbbf1e27b1aeb58370a5ed97709
Eduardo <eduardogm33@outlook>
2026-06-25 16:49:57 +0100

    db:añadidos campos faltantes a la tabla person (dni/nie) y tipo

--- database/migrations/2026_06_09_153951_create_person_table.php
+++ database/migrations/2026_06_09_153951_create_person_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Enums\Type;
 
 return new class extends Migration
 {
@@ -15,8 +16,12 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('surname');
+            $table->string('passport')->unique();
             $table->string('email')->unique();
             $table->string('phone')->unique();
+            $table->string('brand')->nullable();
+            $table->enum('type', array_column(Type::cases(), 'value'))->nullable();
+
             $table->foreignId('client_portfolio_id')->nullable()->constrained('client_portfolio')->nullOnDelete();
             $table->timestamps();
         });
