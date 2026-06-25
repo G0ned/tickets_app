90d8fe1345cd813d743b99b559de1904f04a410e
Eduardo <eduardogm33@outlook>
2026-06-25 16:49:04 +0100

    feat: creada relación entre personas/asistentes y la edicion

--- app/Models/Edition.php
+++ app/Models/Edition.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Edition extends Model
@@ -29,6 +30,13 @@ class Edition extends Model
     {
         return $this->belongsToMany(User::class, 'manager_edition', 'edition_id', 'manager_id')->withPivot(
             'is_supervisor', 'is_doorman', 'invitations_capacity'
+        );
+    }
+
+    public function attendees(): BelongsToMany
+    {
+        return $this->belongsToMany(Person::class, 'attendee_edition', 'edition_id', 'attendee_id')->withPivot(
+            'auth_for_ad', 'auth_for_comms', 'auth_image_rights', 'privacy_policy', 'attendance', 'checked_in_at'
         );
     }
 
--- app/Models/Person.php
+++ app/Models/Person.php
@@ -4,7 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use App\Enums\Type;
+
 
 class Person extends Model
 {
@@ -30,5 +32,12 @@ class Person extends Model
     public function portfolio(): BelongsTo
     {
         return $this ->belongsTo(ClientPortfolio::class, 'client_portfolio_id');
+    }
+
+    public function edition(): BelongsToMany
+    {
+        return $this->belongsToMany(Edition::class, 'attendee_edition', 'attendee_id', 'edition_id')->withPivot(
+            'auth_for_ad', 'auth_for_comms', 'auth_image_rights', 'privacy_policy', 'attendance', 'checked_in_at'
+        );
     }
 }
