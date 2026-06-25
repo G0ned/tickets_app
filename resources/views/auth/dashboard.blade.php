cb0ec8d22a092b335373fe746f3709c0c9db38aa
Eduardo <eduardogm33@outlook>
2026-06-25 09:11:41 +0100

    feat: añadida tarjeta para acceder a todas las listas de invitaciones que gestiona un usuario

--- resources/views/auth/dashboard.blade.php
+++ resources/views/auth/dashboard.blade.php
@@ -23,5 +23,16 @@
                 <p class="text-white text-center text-lg mx-2">Eventos/Ediciones</p>
                 </div>
             </a>
+
+            @if($user->portfolios()->has('invitationLists')->exists())
+                <a href='{{route('invitation-lists-index', $user->id)}}' class="block">
+                    <div class="flex flex-inline items-center bg-gray-500 rounded-lg shadow-sm hover:shadow-md transition-shadow duration-200 p-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" class="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Zm6-10.125a1.875 1.875 0 1 1-3.75 0 1.875 1.875 0 0 1 3.75 0Zm1.294 6.336a6.721 6.721 0 0 1-3.17.789 6.721 6.721 0 0 1-3.168-.789 3.376 3.376 0 0 1 6.338 0Z" />
+                        </svg>
+                        <p class="text-white text-center text-lg mx-2">Listas de Invitados</p>
+                    </div>
+                </a>
+            @endif
     </div>
 </x-layout>
