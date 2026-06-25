8c2c57b1e553015ea676f0b4ecd834fc9d47735f
Eduardo <eduardogm33@outlook>
2026-06-25 09:49:19 +0100

    feat: creado controlador para manejar el formulario de registro. Creada función para mostrar el formulario

---
+++ app/Http/Controllers/FormController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Event;
+
+class FormController extends Controller
+{
+    public function create(Event $event)
+    {
+        return view('form.create')->with('event', $event);
+    }
+}
