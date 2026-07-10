47ba974880780e199ea074d72ef3da70158efa1e
g0ned <eduardogm33@outlook.es>
2026-07-10 13:20:19 +0200

    view: incluidos mensajes de error del formulario

--- resources/views/form/_attendee-fields.blade.php
+++ resources/views/form/_attendee-fields.blade.php
@@ -1,11 +1,11 @@
 @php($submitLabel ??= 'Registrar Asistente')
 
 <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 p-2 sm:p-4">
-    @if (session('error'))
+    @error('error')
         <div class="alert alert-danger">
-            {{ session('error') }}
+            {{ $message }}
         </div>
-    @endif
+    @enderror
     <div>
         <label for="id_type">Tipo de Identificación</label>
         <select
@@ -30,7 +30,7 @@
             class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
             placeholder="00000000X"
             required/>
-        <x-form-error name="id" />
+        <x-form-error name="identification" />
     </div>
 
     <div>
@@ -101,7 +101,7 @@
 <hr class="my-6 border-gray-300" />
 <div class="space-y-6 mt-8">
     <div class="text-sm text-justify px-0 sm:px-2">
-            <p>
+            <p class="px-4">
                 Solicitamos su consentimiento para tratar los datos con las finalidades relacionadas
                 a continuación. La base jurídica que legitima el tratamiento es su consentimiento explícito
                 y se entenderá prestado a través de la marcación de la casilla correspondiente.
@@ -115,16 +115,17 @@
         <div class="ml-3 text-sm w-full">
             <label for="img_rights_ads">Autorización para publicidad</label>
             <div class="flex flex-col sm:flex-row sm:items-center gap-2">
-                <select id="img_rights_ads" name="img_rights_ads" value="{{ old('img_rights_ads') }}" class="block w-full sm:w-auto rounded-md border-gray-700 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm">
-                    <option value="" disabled selected>Elija una opción</option>
-                    <option value="0">No</option>
-                    <option value="1">Sí</option>
+                <select id="img_rights_ads" name="img_rights_ads" class="block w-full sm:w-auto rounded-md border-gray-700 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm" required>
+                    <option value="" disabled {{ old('img_rights_ads') === null ? 'selected' : '' }}>Elija una opción</option>
+                    <option value="0" {{ old('img_rights_ads') === '0' ? 'selected' : '' }}>No</option>
+                    <option value="1" {{ old('img_rights_ads') === '1' ? 'selected' : '' }}>Sí</option>
                 </select>
-                <p class="text-gray-500 sm:ml-2 text-justify">
+                <p class="text-gray-500 sm:ml-2 text-justify px-2">
                     Envío de comunicaciones comerciales por medios electrónicos relacionadas con actividades,
                     productos y eventos de DISTRIBUCIONES EUROCOS, S.L.U.
                 </p>
             </div>
+            <x-form-error name="img_rights_ads" />
         </div>
     </div>
 
@@ -134,14 +135,15 @@
         <div class="ml-3 text-sm w-full">
             <label for="img_rights_web" >Autorización comunicaciones</label>
             <div class="flex flex-col sm:flex-row sm:items-center gap-2">
-                <select id="img_rights_web" name="img_rights_web" value="{{ old('img_rights_web') }}" class="block w-full sm:w-auto rounded-md border-gray-700 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm">
-                    <option value="" disabled selected>Elija una opción</option>
-                    <option value="0">No</option>
-                    <option value="1">Sí</option>
+                <select id="img_rights_web" name="img_rights_web" class="block w-full sm:w-auto rounded-md border-gray-700 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm" required>
+                    <option value="" disabled {{ old('img_rights_web') === null ? 'selected' : '' }}>Elija una opción</option>
+                    <option value="0" {{ old('img_rights_web') === '0' ? 'selected' : '' }}>No</option>
+                    <option value="1" {{ old('img_rights_web') === '1' ? 'selected' : '' }}>Sí</option>
                 </select>
-                <p class="text-gray-500 sm:ml-2 text-justify">Envío por DISTRIBUCIONES EUROCOS, S.L.U. de comunicaciones posteriores al Evento
+                <p class="text-gray-500 sm:ml-2 text-justify px-2">Envío por DISTRIBUCIONES EUROCOS, S.L.U. de comunicaciones posteriores al Evento
                     para la realización de encuestas de satisfacción y acciones de seguimiento.</p>
             </div>
+            <x-form-error name="img_rights_web" />
         </div>
     </div>
 
@@ -151,15 +153,16 @@
         <div class="ml-3 text-sm w-full">
             <label for="img_rights_rss" >Autorización redes sociales</label>
             <div class="flex flex-col sm:flex-row sm:items-center gap-2">
-                <select id="img_rights_rss" name="img_rights_rss" value="{{ old('img_rights_rss') }}" class="block w-full sm:w-auto rounded-md border-gray-700 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm">
-                    <option value="" disabled selected>Elija una opción</option>
-                    <option value="0">No</option>
-                    <option value="1">Sí</option>
+                <select id="img_rights_rss" name="img_rights_rss" class="block w-full sm:w-auto rounded-md border-gray-700 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm" required>
+                    <option value="" disabled {{ old('img_rights_rss') === null ? 'selected' : '' }}>Elija una opción</option>
+                    <option value="0" {{ old('img_rights_rss') === '0' ? 'selected' : '' }}>No</option>
+                    <option value="1" {{ old('img_rights_rss') === '1' ? 'selected' : '' }}>Sí</option>
                 </select>
-                <p class="text-gray-500 sm:ml-2 text-justify">Captar y publicar su imagen / voz durante el Evento con fines de comunicación y
+                <p class="text-gray-500 sm:ml-2 text-justify px-2">Captar y publicar su imagen / voz durante el Evento con fines de comunicación y
                     difusión en los canales corporativos de DISTRIBUCIONES EUROCOS, S.L.U. (Página Web, RRSS, entre otros)
                 </p>
             </div>
+            <x-form-error name="img_rights_rss" />
         </div>
     </div>
 
@@ -168,17 +171,17 @@
         </div>
         <div class="ml-3 text-sm w-full">
             <div class="flex flex-col sm:flex-row sm:items-center gap-2">
-                <select id="privacy_policy" name="privacy_policy" value="{{ old('privacy_policy') }}" class="block w-full sm:w-auto rounded-md border-gray-700 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm" required>
-                    <option value="" disabled selected>Elija una opción</option>
-                    <option value="0">No</option>
-                    <option value="1">Sí</option>
+                <select id="privacy_policy" name="privacy_policy" class="block w-full sm:w-auto rounded-md border-gray-700 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm" required>
+                    <option value="" disabled {{ old('privacy_policy') === null ? 'selected' : '' }}>Elija una opción</option>
+                    <option value="0" {{ old('privacy_policy') === '0' ? 'selected' : '' }}>No</option>
+                    <option value="1" {{ old('privacy_policy') === '1' ? 'selected' : '' }}>Sí</option>
                 </select>
                 <p class="font-bold sm:ml-2">He leído y acepto la <a href="{{ route('privacy-policy') }}" target="_blank" class="text-blue-500 hover:underline"> Política de Privacidad</a> y el Aviso Legal </p>
             </div>
             <x-form-error name="privacy_policy" />
         </div>
     </div>
-<div class="text-sm text-justify mb-4 flex items-start p-0 sm:p-2">
+<div class="text-sm text-justify mb-4 flex items-start px-2 sm:p-2">
        <strong class="pr-4">Atención:</strong> Para enviar el formulario debe activar las casillas según corresponda.
        Declara haber sido informado sobre la Política de Privacidad y el Aviso Legal, aceptando y consintiendo el
        tratamiento de los datos por DISTRIBUCIONES EUROCOS, S.L.U. en la forma y con las finalidades descritas.
