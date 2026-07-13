aa8b442a30b5975a9073b91169d598cbff70f5ad
g0ned <eduardogm33@outlook.es>
2026-07-13 16:56:58 +0200

    test: incluidos tests de eliminacion de eventos

--- tests/Feature/EventRoutesTest.php
+++ tests/Feature/EventRoutesTest.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\User;
 use App\Models\Event;
+use App\Models\Edition;
 
 class EventRoutesTest extends TestCase
 {
@@ -15,6 +16,7 @@ class EventRoutesTest extends TestCase
 
     public function setUp():void
     {
+        //Creacion de usuario para todos los tests
         parent::setUp();
 
         $this->user = User::create([
@@ -28,6 +30,8 @@ class EventRoutesTest extends TestCase
     }
     public function test_event_index_access_only_auth(): void
     {
+        //Prueba para verificar que solo los usuarios identificados (que han hecho log-in) pueden acceder
+        //a la ruta
         $this->actingAs($this->user);
         $response = $this->actingAs($this->user)->get(route('events-create'));
         $response->assertStatus(200);
@@ -35,12 +39,16 @@ class EventRoutesTest extends TestCase
 
     public function test_event_creation_route_as_admin(): void
     {
+        //Prueba para verificar que solo los usuarios administradores (user->is_admin == true) pueden acceder
+        //a la ruta
         $response = $this->actingAs($this->user)->get(route('events-create'));
         $response->assertStatus(200);
     }
 
     public function test_event_creation_route_as_not_admin(): void
     {
+        //Verificar que solos los usuarios administradores pueden acceder a la ruta que carga el formulario
+        //para crear eventos nuevos
         $this->user->is_admin = false;
         $response = $this->actingAs($this->user)->get(route('events-create'));
         $response->assertStatus(403);
@@ -48,6 +56,7 @@ class EventRoutesTest extends TestCase
 
     public function test_event_creation(): void
     {
+        //Verificar que la creacion de un evento nuevo persiste los datos en la BD
         $response = $this->actingAs($this->user)->post(route('events-store'), [
             'name' => 'Demo Name',
             'desc' => 'Demo Event Description',
@@ -61,6 +70,7 @@ class EventRoutesTest extends TestCase
 
     public function test_event_creation_without_user(): void
     {
+        //Verificar que un evento no puede crearse si no tiene un usuario creador (user->events)
         $response = $this->post(route('events-store'), [
             'name' => 'Demo Name',
             'desc' => 'Demo Event Description',
@@ -75,7 +85,7 @@ class EventRoutesTest extends TestCase
     }
 
     public function test_event_editing(): void
-    {   
+    {   //Verificar que el acceso a la edicion de un evento solo es posible si el usuario esta identificado
         $event = Event::create([
             'name' => 'Demo Event Name',
             'description' => 'Demo Event Description',
@@ -89,6 +99,7 @@ class EventRoutesTest extends TestCase
 
     public function test_event_updating(): void
     {
+        //Verificar que la actualizacion de los datos de un evento persiste los cambios en la BD
         $event = Event::create([
             'name' => 'Demo Event Name',
             'description' => 'Demo Event Description',
@@ -104,5 +115,92 @@ class EventRoutesTest extends TestCase
         ]);
 
         $this->assertDatabaseHas('events', ['name' => 'New Demo Event Name']);
+    }
+
+    public function test_event_delete(): void
+    {
+        //Verificar que la eliminacion de un evento lo marca como eliminado
+        $event = Event::create([
+            'name' => 'Demo Event Name',
+            'description' => 'Demo Event Description',
+            'public' => false,
+            'created_by' => $this->user->id,
+        ]);
+
+        $response = $this->actingAs($this->user)->delete(route('events-delete', $event->id));
+        $this->assertSoftDeleted('events', [
+            'id' => $event->id
+        ]);
+    }
+
+    public function test_event_with_pending_editions_is_not_deleted(): void
+    {
+        $event = Event::create([
+            'name' => 'Demo Event Name',
+            'description' => 'Demo Event Description',
+            'public' => false,
+            'created_by' => $this->user->id,
+        ]);
+
+        $edition1 = Edition::create([
+                'event_id' => $event->id,
+                'date' => '2026-07-12 10:00:00',
+                'duration' => 1,
+                'location' => 'demoPlace',
+                'capacity' => 999,
+                'status' => false
+
+            ]);
+
+        $edition2 = Edition::create([
+            'event_id' => $event->id,
+            'date' => '9999-12-29 00:00:00',
+            'duration' => 1,
+            'location' => 'demoPlace',
+            'capacity' => 999,
+            'status' => false
+
+        ]);
+        $this->actingAs($this->user)->delete(route('events-delete', $event->id));
+
+        $this->assertDatabaseHas('events', [
+            'id' => $event->id,
+            'deleted_at' => null
+        ]);
+    }
+
+    public function test_event_with_celebrated_editions_is_deleted(): void
+    {
+         $event = Event::create([
+            'name' => 'Demo Event Name',
+            'description' => 'Demo Event Description',
+            'public' => false,
+            'created_by' => $this->user->id,
+        ]);
+
+        $edition1 = Edition::create([
+                'event_id' => $event->id,
+                'date' => '2026-07-12 10:00:00',
+                'duration' => 1,
+                'location' => 'demoPlace',
+                'capacity' => 999,
+                'status' => false
+
+            ]);
+
+        $edition2 = Edition::create([
+            'event_id' => $event->id,
+            'date' => '2026-06-12 10:00:00',
+            'duration' => 1,
+            'location' => 'demoPlace',
+            'capacity' => 999,
+            'status' => false
+
+        ]);
+        $this->actingAs($this->user)->delete(route('events-delete', $event->id));
+
+        $this->assertSoftDeleted('events', [
+            'id' => $event->id
+        ]);
     }
 }
