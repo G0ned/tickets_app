17d84bde26d1425a4fe2eec4f2fdc54c98e8c21c
g0ned <eduardogm33@outlook.es>
2026-09-15 14:07:08 +0200

    refactor: El cambio de contraseña se ha movido a la página de dashboard en lugar del navbar

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -49,11 +49,6 @@
                     </svg>
                   </x-nav_link>
                 @endif
-                <x-nav_link href="{{ route('account-password-edit') }}" :active="request()->routeIs('account-password-edit')" title="Cambiar contraseña">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z" />
-                  </svg>
-                </x-nav_link>
                 <form method="POST" action="/logout" class="inline">
                   @csrf
                   <button type="submit" class="text-white hover:bg-teal-700 px-3 py-2 rounded-md text-sm font-medium">Logout</button>
@@ -90,7 +85,6 @@
             <a href="{{ url('/dashboard') }}" class="block text-white hover:bg-teal-700 px-3 py-2 rounded-md text-base font-medium">Dashboard</a>
           @endadmin
           @auth
-            <a href="{{ route('account-password-edit') }}" class="block text-white hover:bg-teal-700 px-3 py-2 rounded-md text-base font-medium">Cambiar contraseña</a>
             <form method="POST" action="/logout">
               @csrf
               <button type="submit" class="w-full text-left text-white hover:bg-teal-700 px-3 py-2 rounded-md text-base font-medium">Logout</button>
